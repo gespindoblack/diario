2b8de9d105165567a3c1f5177614b98e42dd1c9c
Guillermo Espindola <guillermo.espindola@gmail.com>
2011-05-27 17:20:34 -0300

    @new: my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diario::Application.config.secret_token = 'f7fceb4a70a11fc47986d22e0294f33dca44dbfe9f3494830641d35e02e21b595cbddfa664a01e5e75445ceb6a25f362e1701809e4f0b6a76a9c305fb39d1b4f'
